c4dbb3de06cf6b334eb1f86fe7e5852417a38952
Babu Srinivasan <babu.srinivasan@mongodb.com>
2024-05-08 14:19:56 +0530

    updated the lamdba stack

--- cdk/cms-cdk/AWS_EventBridge_Stack/AWS_EventBridge_Stack.py
+++ cdk/cms-cdk/AWS_EventBridge_Stack/AWS_EventBridge_Stack.py
@@ -1,7 +1,7 @@
 from constructs import Construct
 from aws_cdk import (
     RemovalPolicy,
-    aws_events as event,
+    aws_events as events,
     aws_lambda as _lambda,
     aws_events_targets as targets,
     Stack,
@@ -13,24 +13,14 @@ from parameters.global_args import GlobalArgs
 
 class EventBridgeStack(Stack):
 
-       def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
- # Define a new Lambda resource
-        my_lambda = _lambda.Function(
-            self, "MyLambda",
-            code=_lambda.Code.from_inline("exports.handler = function(event, ctx, cb) { return cb(null, \"hi\"); }"),
-            handler="index.handler",
-            runtime=_lambda.Runtime.NODEJS_10_X,
+        # Define a new Lambda resource
+        events.CfnEventBus(self, 'PartnerEventBus',
+        name='partner_event_bus_name',
+        event_source_name='partner_name'
         )
 
-        # Define a new EventBridge rule
-        rule = events.Rule(
-            self, "Rule",
-            event_pattern=events.EventPattern(
-                source=["aws.ec2"]
-            )
-        )
 
-        # Add Lambda as a target to the EventBridge rule
-        rule.add_target(targets.LambdaFunction(my_lambda))
+        
--- cdk/cms-cdk/AWS_Lambda_Stack/AWS_Lambda_Stack.py
+++ cdk/cms-cdk/AWS_Lambda_Stack/AWS_Lambda_Stack.py
@@ -1,24 +1,73 @@
 from constructs import Construct
 from aws_cdk import (
     RemovalPolicy,
-    aws_ecr_assets as ecr,
+    aws_lambda as _lambda,
     Stack,
     CfnOutput,
     Tags,
-    App
+    App,
+    aws_secretsmanager as secretsmanager,
+    SecretValue as SecretValue
 )
 
 from parameters.global_args import GlobalArgs
 
+env_name = "dev"
+secretname = "CMS_ATLAS_URL"
+
+
+
+
+# Create a new lambda function pull_from_mdb
 class LambdaStack(Stack):
 
-       def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+       def __init__(self, scope: Construct, construct_id: str, atlas_uri : str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
+        # Create a new secret in Secrets Manager and fetch the ARN
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "CmsCdkQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        mySecret = secretsmanager.Secret(self, "Secret", secret_name = secretname, secret_string_value = SecretValue.unsafe_plain_text(atlas_uri))
+
+        secretarn = mySecret.secret_arn
+        
+        generated_name = mySecret.secret_name
+
+        # Create a new lambda function to pull data from MongoDB
+        lambda_function_pull_from_mdb = _lambda.Function(
+            self, "pull_from_mdb",
+            runtime=_lambda.Runtime.PYTHON_3_12,
+            handler="lambda_pull_from_mdb.handler",
+            code=_lambda.Code.from_asset("AWS_Lambda_Stack"),
+            environment={
+                "PYTHONPATH" : "dependencies",
+                "LOG_LEVEL": "INFO",
+                "APP_ENV": "dev",
+                "REGION_NAME":"us-east-1",  # Update your region
+                "EVENTBUS_NAME":"XXXXX",  # Update the event-bus created 
+                "MODEL_ENDPOINT":"sagemaker-soln-XXXX"  # Update your sagemaker model endpoint
+            }
+        )
+
+        secret = secretsmanager.Secret.from_secret_attributes(self, secretname, 
+        secret_complete_arn=secretarn)
+        
+        secret.grant_read(grantee=lambda_function_pull_from_mdb)
+
+
+        # Create a new lambda function to push data to MongoDB
+        lambda_function_push_to_mdb = _lambda.Function(
+            self, "push_to_mdb",
+            runtime=_lambda.Runtime.PYTHON_3_12,
+            handler="lambda_push_to_mdb.handler",
+            code=_lambda.Code.from_asset("AWS_Lambda_Stack"),
+            environment={
+                "PYTHONPATH" : "dependencies",
+                "LOG_LEVEL": "INFO",
+                "APP_ENV": "dev",
+                "region_name":"us-east-1",
+                "evnetbus_name":"default",
+                "model_endpoint":"sagemaker-soln-XXXX"
+            }
+        )
+        
+        secret.grant_read(grantee=lambda_function_push_to_mdb)
